b59d772766d19c8377af53316e36aa7ff1e50b9e
OHironari <hiroball0728@gmail.com>
2025-05-06 15:12:12 +0900

    Merge remote and local changes

---
+++ dynamoDB.tf
@@ -0,0 +1,14 @@
+# ===========
+# Define Dynamo DB 
+# ===========
+resource "aws_dynamodb_table" "items" {
+  name         = "ItemsTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
